72123457efa9dac780fdd7a70222211f6b0e19c4
Yan Fernandes <102800044+yan-pi@users.noreply.github.com>
2024-05-03 16:45:12 -0300

    feat: add react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+import './index.css'
+import AuthPage from './routes/auth-page.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <AuthPage />,
+    children: [
+      {
+        path: "/",
+        //element: <Home />,
+      },
+      {
+        path: "/repos/:username",
+        //element: <Repos />,
+      },
+    ],
+  },
+]);
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
